fba1b6601936cea59bfb9f349b2004ff6f4722f4
Prashant Singh <prashantsingh5aug@gmail.com>
2022-05-18 23:11:19 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const connectToMongo = require('./db');
 connectToMongo();
 
-
 let cors = require('cors')
 const express = require('express')
 const app = express()
-const port = 8000;
+const PORT = 8000;
 
 app.use(cors())
 app.use(express.json())
@@ -20,6 +19,6 @@ app.use('/api/auth', require('./routes/cart'))
 app.use('/api/auth', require('./routes/item'))
 app.use('/api/auth', require('./routes/order'))
 
-app.listen(port, () => {
-  console.log(`Bigmart running at http://localhost:${port}`)
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Bigmart running at http://localhost:${PORT}`)
 })
